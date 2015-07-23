d3988285db636e2151309e07790ca2fab3d5f0b9
Veronica Rivera <damediabolique@gmail.com>
2015-07-23 00:08:57 -0400

    Added Mobile Nav to index.html and Javascript, Added additional Media Queries

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,25 @@ $(".datepicker").datepicker();
 	//Seniors: Optional. User selects the amount of seniors.
 		//Seniors get 10% discount off the Cost of Trip
 	//Submit
+	$('#bookForm').submit( function ( submit ) {
+		submit.preventDefault();
+		console.log($('#departCity').attr('value'));
+		console.log($('#arriveCity').attr('value'));
+		console.log($('#departDate').attr('value'));
+		console.log($('#returnDate').attr('value'));
+		console.log($('#adults').attr('value'));
+		console.log($('#seniors').attr('value'));
+	});
 		//Add a Lightbox Overlay displaying data inserted by User
 			//Confirm
 				//Thank you Message appears
 			//Cancel
 				//User is directed back to the form and overlay is removed
 		
+//Mobile Navigation
+	//Mobile Nav Menu hidden on load
+	$('#navlist').hide();
+	//When User Clicks or Taps on the Menu Bars, the Nav Menu Appears and can toggle on and off
+	$('#switch').click( function () {
+		$('#navlist').toggle('blind');
+	});
