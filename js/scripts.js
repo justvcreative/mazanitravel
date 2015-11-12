f665509d72d6b4a38ce48acc1b072aa242d92d1b
unknown <damediabolique@gmail.com>
2015-11-11 20:16:10 -0500

    Changed Nav, Gallery, Form

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,17 @@
 			var returndate = $('#returnDate').prop('value'); // store Return Date
 			var adults = $('#adults').prop('value'); // store Amount of Adults
 			var seniors = $('#seniors').prop('value'); // store Amount of Seniors
+			// var confirmIMG = '<img src="img/resortconfirm.jpg" alt="Pool Bar" class="img-responsive hidden-xs hidden-sm">'
 			$('#confirm').show(); // Show Confirm Section
 
 			$('#confirm').html( 
 				//Add following html markup and displays data stored in variables
-				'<hr>' +
-				'<h2>Your Next Adventure Awaits!</h2>' +
-				'<h3>Please Confirm your Package</h3>' +
-				'<div>' +
+				'<div class="row">' +
+						'<div class="col-sm-6" id="confirmIMG">' +
+						'</div>'+
+						'<div class="col-sm-6" id="confirmMSG">' +
+						'<h2>Your Next Adventure Awaits!</h2>' +
+						'<h3>Please Confirm your Package</h3>' +
 						'<h4>Departure City</h4>'+
 						'<p>' + departcity +' </p>' + // display Return Date
 						'<h4>Destination</h4>' +
@@ -30,26 +33,20 @@
 						'<p>' + returndate + '</p>'+ // display Return Date
 						'<h4>Travelers</h4>' +
 						'<p>Adults: ' + adults + ' | Seniors: ' + seniors + '</p>' +
-				'</div>' +			
-				'<div id="confirmationButtons"><button type="button" id="confirmButton" class="btn jumbo-btn btn-lg">Confirm</button>' +
-				'<button type="button" id="cancelButton" class="btn jumbo-default-btn btn-lg">Cancel</button></div><hr>' // Confirmation Buttons
+						'<div id="confirmationButtons">' +
+						'<button type="button" id="confirmButton" class="btn jumbo-btn btn-lg">Confirm</button>' +
+						'<button type="button" id="cancelButton" class="btn jumbo-default-btn btn-lg">Cancel</button></div>' + // Confirmation Buttons
+						'</div>'+
+				'</div>'		
 				);
 
 					$('#confirmButton').click(function () {
 						$('#confirmationButtons button').hide(); // Confirmation Buttons are removed
-						$('#confirm').append(
-								'<h2 style="color: #8DC63F">Thank You For booking your Adventure to<br>' +
-								destination + '!</h2>'); // Thank You Message Appears
+						$('#confirmMSG').append(
+								'<p class="successMSG">Thank You For booking your Adventure to<br><strong>' +
+								destination + '!</strong></p>'); // Thank You Message Appears
 					});
 					$('#cancelButton').click(function () {
 						$('#confirm').hide(); // Hide Confirm Section
 					});
 		});
-
-//Mobile Navigation
-	//Mobile Nav Menu hidden on load
-	$('#navlist').hide();
-	//When User Clicks or Taps on the Menu Bars, the Nav Menu Appears and can toggle on and off
-	$('#switch').click( function () {
-		$('#navlist').toggle('blind');
-	});
